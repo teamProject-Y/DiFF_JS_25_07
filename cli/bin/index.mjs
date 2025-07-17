0a5b9735a0799d028b08873e63643f1734063797
yunzivv <yunzivv@gmail.com>
2025-07-18 00:53:12 +0900

    CLI 명령어 인식까지 완료

---
+++ cli/bin/index.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import chalk from 'chalk';
+
+const program = new Command();
+
+program
+    .name("git-mkdraft")
+    .description("Git 커밋 기반 블로그 초안 생성 CLI")
+    .argument('<branch>', '분석할 브랜치 이름')
+    .option('--no-title', '제목 제외')
+    .option('--no-filename', '파일명 제외')
+    .option('--full-code', '변경 전 코드 포함')
+    .option('--last-only', '첫커밋과 마지막 커밋만 추적')
+    .action((branch, options) => {
+        console.log("Making to draft...");
+        // console.log('*', chalk.green(branch));
+        console.log('Options:', options);
+        console.log('done.');
+    });
+
+program.parse();
