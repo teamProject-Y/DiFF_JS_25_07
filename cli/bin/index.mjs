1f71a15b9d96b6a7fb682ca9108d2d291b657863
yunzivv <yunzivv@gmail.com>
2025-07-19 13:24:12 +0900

    console message 수정

--- cli/bin/index.mjs
+++ cli/bin/index.mjs
@@ -13,7 +13,7 @@ async function getGitEmail() {
         const email = execSync('git config user.email').toString().trim();
         return email;
     } catch (err) {
-        console.error(chalk.red('Git 이메일을 불러올 수 없습니다.'));
+        console.error(chalk.red('\n' + 'You can use it after login to git'));
         return null;
     }
 }
@@ -28,7 +28,7 @@ async function verifyGitUser(email) {
         console.log(email);
         return res.data.verified;
     } catch (err) {
-        console.error(chalk.red('서버 인증 요청 실패:'), err.message);
+        console.error(chalk.red('error:'), err.message);
         return false;
     }
 }
@@ -43,26 +43,26 @@ program
     .option('--last-only', '첫커밋과 마지막 커밋만 추적')
     .action(async (branch, options) => {
 
-        console.log(chalk.blue(`\nGit 사용자 인증 중...`));
+        console.log(`\nGit user verfying...`);
 
         const email = await getGitEmail();
         if (!email) {
-            console.error(chalk.red('Git 사용자 이메일을 찾을 수 없습니다.'));
+            console.error(chalk.red('Git user email not found.'));
             process.exit(1);
         }
 
         const verified = await verifyGitUser(email);
         if (!verified) {
-            console.error(chalk.red(`등록되지 않은 사용자입니다: ${email}`));
+            console.error(chalk.red(`You are an unregistered user: ${email}`));
             process.exit(1);
         }
 
-        console.log(chalk.green(`인증된 사용자: ${email}\n`));
+        console.log('User authentication completed');
 
-        console.log("Making to draft...");
+        console.log('Making to draft...');
         // console.log('*', chalk.green(branch));
         console.log('Options:', options);
-        console.log('done.');
+        // console.log('done.');
     });
 
 program.parse();
