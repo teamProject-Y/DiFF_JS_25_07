e8f06d48c771324b8ce046d776e3765889feed36
Yullc <apanan0910@gmail.com>
2025-08-07 22:15:20 +0900

    memberId Repo star article output

--- client/src/lib/ArticleAPI.js
+++ client/src/lib/ArticleAPI.js
@@ -6,9 +6,9 @@ import { UserApi } from './UserAPI';
 
 // lib/ArticleAPI.js
 
-export const fetchArticles = async ({ searchItem = 0, keyword = "", page = 1 }) => {
-    const res = await UserApi.get(`/api/DiFF/article/list`, {
-        params: { searchItem, keyword, page }
+export const fetchArticles = async ({ repositoryId, searchItem = 0, keyword = "", page = 1 }) => {
+    const res = await UserApi.get('/api/DiFF/article/list', {
+        params: { repositoryId, searchItem, keyword, page }
     });
     return res.data;
 };
