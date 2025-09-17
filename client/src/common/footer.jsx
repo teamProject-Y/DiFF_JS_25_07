27bbb562bc5764b87ca9cc4be69ca3bdadc399fb
yeji-star <sizuku134@gmail.com>
2025-09-17 13:16:21 +0900

    footer

--- client/src/common/footer.jsx
+++ client/src/common/footer.jsx
@@ -1,26 +1,29 @@
 export default function Footer() {
-    const OPS_EMAIL = 'support@diff.io.kr';
+    const OPS_EMAIL = 'diff.io.kr@gmail.com';
     const mailtoHref = `mailto:${OPS_EMAIL}?subject=${encodeURIComponent('[DiFF] 문의')}`;
 
     return (
-        <footer className="bg-gray-800 text-white text-center p-3 text-sm">
-            <p>
+        <footer className="relative bg-gray-100 text-white w-full min-h-[200px] text-center p-3 text-sm">
+            <div className="absolute top-0 inset-x-20 border-t border-gray-500">
+            <p className="mt-10 text-gray-600">
                 email:{' '}
-                <a href={mailtoHref} className="underline hover:no-underline">
+                <a href={mailtoHref} className="hover:text-gray-400 duration-100">
                     {OPS_EMAIL}
                 </a>
             </p>
-            <p>
+            <br />
+            <p className="text-gray-600">
                 GitHub Issues:{' '}
                 <a
                     href="https://github.com/teamProject-Y/DiFF/issues"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="underline hover:no-underline"
+                    className="hover:text-gray-400 duration-100"
                 >
                     teamProject-Y/DiFF/issues
                 </a>
             </p>
+            </div>
         </footer>
     );
 }
